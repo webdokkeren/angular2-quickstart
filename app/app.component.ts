cdf46ddcfd1dc38fa191d7dde327f40539b14e78
Thor Madsen-Holm <thormadsenholm@gmail.com>
2015-12-16 18:47:33 +0100

    display the list of heroes

--- app/app.component.ts
+++ app/app.component.ts
@@ -11,22 +11,20 @@ interface Hero {
 })
 
 export class AppComponent {
-    public title = 'Tour of Heroes';
-    public hero: Hero = {
-        id: 1,
-        name: 'Kardel Sharpeye'
-    }            
+    public title = 'Tour of Heroes';    
+    public heroes = HEROES;
 }
 
 var HEROES: Hero[] = [
+    { "id": 1, "name": 'KARDEL SHARPEYE'},            
     { "id": 11, "name": "TRAXEX" },
     { "id": 12, "name": "FAERIE DRAGON" },
     { "id": 13, "name": "RAIGOR STONEHOOF" },
     { "id": 14, "name": "DARCHROW" },
-    { "id": 15, "name": "Magneta" },
-    { "id": 16, "name": "RubberMan" },
-    { "id": 17, "name": "Dynama" },
-    { "id": 18, "name": "Dr IQ" },
-    { "id": 19, "name": "Magma" },
-    { "id": 20, "name": "Tornado" }
+    { "id": 15, "name": "KARROCH" },
+    { "id": 16, "name": "ZHARVAKKO" },
+    { "id": 17, "name": "PRINCESS OF THE MOON" },
+    { "id": 18, "name": "LESALE DEATHBRINGER" },
+    { "id": 19, "name": "LYRALEI" },
+    { "id": 20, "name": "RHASTA" }
 ];
