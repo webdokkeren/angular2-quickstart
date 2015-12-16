5a315eabf630c8d3c9e5d3525e319650a28b9ec5
Thor Madsen-Holm <thormadsenholm@gmail.com>
2015-12-16 18:43:18 +0100

    added array of heroes

--- app/app.component.ts
+++ app/app.component.ts
@@ -17,3 +17,16 @@ export class AppComponent {
         name: 'Kardel Sharpeye'
     }            
 }
+
+var HEROES: Hero[] = [
+    { "id": 11, "name": "TRAXEX" },
+    { "id": 12, "name": "FAERIE DRAGON" },
+    { "id": 13, "name": "RAIGOR STONEHOOF" },
+    { "id": 14, "name": "DARCHROW" },
+    { "id": 15, "name": "Magneta" },
+    { "id": 16, "name": "RubberMan" },
+    { "id": 17, "name": "Dynama" },
+    { "id": 18, "name": "Dr IQ" },
+    { "id": 19, "name": "Magma" },
+    { "id": 20, "name": "Tornado" }
+];
