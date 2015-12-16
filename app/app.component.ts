6f19eccc1f5c9008b4aaed199b89a16863ec6667
Thor Madsen-Holm <thormadsenholm@gmail.com>
2015-12-16 18:31:13 +0100

    added interface and updated template

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,19 @@
 import {Component} from 'angular2/core';
 
+interface Hero {
+    id: number;
+    name: string;
+}
+
 @Component({
     selector: 'my-app',
     templateUrl: 'app/app.component.tmpl.html'
 })
 
-export class AppComponent { }
+export class AppComponent {
+    public title = 'Tour of Heroes';
+    public hero: Hero = {
+        id: 1,
+        name: 'Kardel Sharpeye'
+    }            
+}
